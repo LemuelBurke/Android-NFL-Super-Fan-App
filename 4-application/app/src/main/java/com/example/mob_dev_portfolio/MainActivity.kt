9a264c62d0ef9bca48107e270e5f1abf0427748f
c23015833 <burkem3@cardiff.ac.uk>
2025-02-24 13:28:13 +0000

    Added toolbar

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/MainActivity.kt
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.mob_dev_portfolio
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +20,30 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.appbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.home -> {
+                recreate()
+                true
+            }
+            R.id.news -> {
+                recreate()
+                true
+            }
+            else -> super.onOptionsItemSelected(item) // Fallthrough
         }
     }
 }
